1c6a304233d729b16a269a05ec36ceb3aedbe8a1
Hunter <bobnamed123@yahoo.com>
2021-07-12 22:46:03 -0700

    ruby tutorial shouldve studied more

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_011323) do
+ActiveRecord::Schema.define(version: 2021_07_13_042632) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
